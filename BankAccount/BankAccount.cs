51335b8c849e03e148576368f7212b9cb79e9b3e
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-04-21 22:08:24 +0300

    domashna

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiniraneNaKlas
+{
+    internal class BankAccount
+    {
+        public int id;
+        public double balance;
+
+        public int ID
+        {
+            get
+            { 
+                return id; 
+            }
+            set 
+            { 
+                id = value; 
+            }
+        }
+
+        public double Balance
+        {
+            get 
+            { 
+                return Balance; 
+            }
+            set 
+            { 
+                Balance = value; 
+            }
+        }
+        public BankAccount()
+        {
+            this.id = id;
+            this.balance = balance;
+        }
+
+        
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DefiniraneNaKlas
+{
+    internal class Program
+    {
+       public static void Main()
+        {
+            BankAccount acc = new  BankAccount();
+             acc.id = 1;
+            acc.Balance = 15;
+            
+            Console.WriteLine($"Account {acc.id}, balance {acc.Balance}");
+       }
+    }
+}
